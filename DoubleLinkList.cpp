31138c2fa00e3b35330a9d724bff6b7da979db51
ZhilPro27 <zhilalkrisna@gmail.com>
2024-05-04 21:14:04 +0700

    Menambahkan conditional statement untuk mengecek apakah list sudah berisi atau tidak di dalam prosedur deleteNode

--- DoubleLinkList.cpp
+++ DoubleLinkList.cpp
@@ -78,4 +78,9 @@ void deleteNode() {
 	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
 	cin >> rollNo;	// step 3: get the roll number to be deleted
 
+	if (START = NULL) {
+		cout << "List is empty" << endl;
+		return;
+	}
+
 }
